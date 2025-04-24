aeeb4dc9f9e4e7c9d3b6c9ae78c3b6a8137652b1
theiann <2015iann@gmail.com>
2025-04-24 13:41:42 -0500

    Added error handling for connecting to the MySQL server.

--- MedicalDatabase.py
+++ MedicalDatabase.py
@@ -1,9 +1,21 @@
 import mysql.connector
+from mysql.connector import errorcode
 
+
+
+# Password for the SQL server is stored on a .txt file in the same folder as the python script
+# No, this is not secure, but I'd rather not put any sensitive information in my source code either.
 f = open("password.txt", "r")
-print(f.read())
-p = f.read().rstrip()
-#print(p)
-cnx = mysql.connector.connect(user='root', password=p , host='localhost')
-cnx.close()
+p = f.read()
+try:
+    cnx = mysql.connector.connect(user='root' , password=p, host='127.0.0.1')
+except mysql.connector.Error as err:
+  if err.errno == errorcode.ER_ACCESS_DENIED_ERROR:
+    print("Something is wrong with your user name or password")
+  elif err.errno == errorcode.ER_BAD_DB_ERROR:
+    print("Database does not exist")
+  else:
+    print(err)
+else:
+    cnx.close()
 f.close()
